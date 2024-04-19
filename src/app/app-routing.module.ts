4be334e30be8242e38fd01fff611659f0dd2eaa3
kareemmohamed21 <kareem.mohamed.fcess@gmail.com>
2024-04-19 04:47:40 +0200

    CommitChanges

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllproductsComponent } from './products/components/allproducts/allproducts.component';
+import { CartsComponent } from './carts/components/carts/carts.component';
+import { ProductsdetailsComponent } from './products/components/productsdetails/productsdetails.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: AllproductsComponent },
+  { path: 'cart', component: CartsComponent },
+  { path: 'details/:id', component: ProductsdetailsComponent },
+  { path: '**', redirectTo: 'products', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { ProductsService } from './products/services/products.service';
+import { AllproductsComponent } from './products/components/allproducts/allproducts.component';
+import { ProductsdetailsComponent } from './products/components/productsdetails/productsdetails.component';
+import { NewComponent } from './new/new.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllproductsComponent,
+    ProductsdetailsComponent,
+    NewComponent,
+    TestComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    AppRoutingModule,
+    SharedModule,
+          ],
+  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
